54d34fc7884db3ed48e59f7f9e9a7a871705ce78
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-09-01 23:40:10 +0000

    add: basic menu

--- main.go
+++ main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
-	var dlist = doubly - linked - list.ds.DoublyLinkedList
+	fmt.Println("********************************************************")
+	fmt.Println("Data Structures and Algorithms - Tests and Experiments")
+	fmt.Println("********************************************************")
+
+	var input int
+
+	for { // Add error handling for non-int input
+		printMenu()
+		fmt.Scanf("%d", &input)
+		switch input {
+		case 0:
+			fmt.Println("Exiting...")
+			return
+		case 1:
+			fmt.Println("Working")
+		}
+	}
+}
+
+func printMenu() {
+	fmt.Println("Which Data Structure or Algorthim do you want to try out?")
+	fmt.Println("0 - Exit program\n1 - Doubly Linked List")
 }

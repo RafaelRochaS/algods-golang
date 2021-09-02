9921b34828ad3753ce799626b240107539dcf2f5
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-09-02 00:19:10 +0000

    add: basic dlist ops

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"algods/operations"
 	"fmt"
 )
 
@@ -14,18 +15,19 @@ func main() {
 
 	for { // Add error handling for non-int input
 		printMenu()
-		fmt.Scanf("%d", &input)
+		fmt.Scanf("%d", &input) // Input needs a refactor
 		switch input {
 		case 0:
 			fmt.Println("Exiting...")
 			return
 		case 1:
-			fmt.Println("Working")
+			operations.DListMenu()
 		}
 	}
 }
 
 func printMenu() {
-	fmt.Println("Which Data Structure or Algorthim do you want to try out?")
+	fmt.Println("\nWhich Data Structure or Algorthim do you want to try out?")
 	fmt.Println("0 - Exit program\n1 - Doubly Linked List")
+	fmt.Println("---------------------------------------------")
 }
---
+++ operations/doubly-linked-list.ops.go
@@ -0,0 +1,51 @@
+package operations
+
+import (
+	"algods/datastructures"
+	"fmt"
+)
+
+func DListMenu() {
+	dlist := datastructures.DoublyLinkedList{}
+	var input int
+	for {
+		printMenu()
+		fmt.Scanf("%d", &input)
+		switch input {
+		case 0:
+			return
+		case 1:
+			insertItem(&dlist)
+		case 2:
+			fmt.Println("Clearing list...")
+			dlist.Clear()
+		case 3:
+			searchItem(dlist)
+		}
+	}
+}
+
+func insertItem(dlist *datastructures.DoublyLinkedList) {
+	var input int
+	fmt.Print("\nType integer to insert on the list: ")
+	fmt.Scanf("%d", &input)
+	dlist.Insert(input)
+}
+
+func searchItem(dlist datastructures.DoublyLinkedList) {
+	var input int
+	fmt.Print("\nType integer to search on the list: ")
+	fmt.Scanf("%d", &input)
+	_, err := dlist.Find(input)
+	if err != nil {
+		fmt.Println("Item is in the list.")
+	} else {
+		fmt.Println("Item is not on the list.")
+	}
+}
+
+func printMenu() {
+	fmt.Println("Select an operation to perform:")
+	fmt.Println("0 - Return\n1 - Insert\n2 - Clear\n3 - Find\n4 - Delete\n5 - Insert Random\n6 - Traverse")
+	fmt.Println("---------------------------------------------")
+}

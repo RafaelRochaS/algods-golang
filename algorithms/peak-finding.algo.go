1fd2e44c697f48d2f4ab426f2ab4fda240d3d8ed
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-09-25 20:09:18 +0000

    add: 2D peak finding

--- algorithms/peak-finding.algo.go
+++ algorithms/peak-finding.algo.go
@@ -2,14 +2,42 @@
 // the data structures, or be used stand alone
 package algorithms
 
+import "errors"
+
 // Algorithms to solve the Peak Finding problem - 1D version
 func PeakFinding1D(arr []int) int {
 	index := (len(arr) / 2)
-	if arr[index] < arr[index-1] {
-		return PeakFinding1D(arr[:index+1])
-	} else if arr[index] < arr[index+1] {
-		return PeakFinding1D(arr[index:])
+	if index != 0 && arr[index] < arr[index-1] {
+		return PeakFinding1D(arr[:index])
+	} else if index != len(arr)-1 && arr[index] < arr[index+1] {
+		return PeakFinding1D(arr[index+1:])
 	} else {
 		return arr[index]
 	}
+}
+
+// Helper function to grab index of value in array. Kills performance, since in average case runs in O(n)
+func indexOf(value int, arr []int) (int, error) {
+	for k, v := range arr {
+		if v == value {
+			return k, nil
+		}
+	}
+	return -1, errors.New("item not found")
+}
+
+func PeakFinding2D(matrix [][]int) (int, error) {
+	middle := len(matrix) / 2
+	// the line below prevents the algorithm from running in O(log(nm)), since finding the peak in a column returns a value, not an index, and it is necessary to get the index of that value.
+	// To fix that, peak finding 1D must be ran inside this function, which would make a big mess. The whole algorithm would have to be ran in a loop, instead of recursively.
+	if peak1D, err := indexOf(PeakFinding1D(matrix[middle]), matrix[middle]); err == nil {
+		if middle != 0 && matrix[middle-1][peak1D] > matrix[middle][peak1D] {
+			return PeakFinding2D(matrix[0:middle])
+		} else if middle != len(matrix)-1 && matrix[middle+1][peak1D] > matrix[middle][peak1D] {
+			return PeakFinding2D(matrix[middle+1:])
+		} else {
+			return matrix[middle][peak1D], nil
+		}
+	}
+	return -1, errors.New("failed to compute")
 }
--- operations/peak-finding.ops.go
+++ operations/peak-finding.ops.go
@@ -10,13 +10,16 @@ func PeakFindingMenu() {
 	var input int
 	for {
 		common.PrintMenu(`0 - Return
-1 - 1D Random`)
+1 - 1D Random
+2 - 2D Random`)
 		fmt.Scanf("%d", &input)
 		switch input {
 		case 0:
 			return
 		case 1:
 			randomPeak1D()
+		case 2:
+			randomPeak2D()
 		}
 	}
 }
@@ -24,4 +27,15 @@ func PeakFindingMenu() {
 func randomPeak1D() {
 	arr := []int{6, 7, 4, 3, 2, 1, 4, 5}
 	fmt.Println(algorithms.PeakFinding1D(arr))
+}
+
+func randomPeak2D() {
+	matrix := [][]int{
+		{1, 2, 3},
+		{19, 9, 10},
+		{7, 8, 9},
+		{10, 7, 1},
+		{13, 2, 11}}
+	result, _ := algorithms.PeakFinding2D(matrix)
+	fmt.Println(result)
 }
